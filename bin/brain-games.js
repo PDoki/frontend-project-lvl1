6817a57f74429f09c8de38fc7c11dca72e05228a
PDoki <polinaclass2018@gmail.com>
2020-02-15 13:02:08 +0200

    step3 added readline-sync lib and ask user name

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,7 @@
 #!/usr/bin/env node
-console.log("Welcome to the Brain Games!");
+import { userName } from '../src/index.js';
+
+const greeting = "Welcome to the Brain Games!";
+console.log(greeting);
+const askUserName = userName();
+console.log(askUserName);
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+const userName = () => {
+  const user = readlineSync.question('May I have your name, please? ');
+  console.log(`Hello, ${user}!`);
+};
+
+export { userName };
